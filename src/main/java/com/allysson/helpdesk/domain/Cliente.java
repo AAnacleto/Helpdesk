41eb8f0f7eb5bb0cca7f79a087e71947f9dd17bd
C1325053 <allysson.oliveira@capgemini.com>
2022-04-28 01:15:17 -0300

    anotacoes_jpa_para_class

--- src/main/java/com/allysson/helpdesk/domain/Cliente.java
+++ src/main/java/com/allysson/helpdesk/domain/Cliente.java
@@ -3,16 +3,29 @@ package com.allysson.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.allysson.helpdesk.domain.enuns.Perfil;
+
+@Entity
 public class Cliente extends Pessoa {
 	
-	private List<Chamado> chamados = new ArrayList<>();
+	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
+    private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfis(Perfil.CLIENTE);
+
 	}
 
 	public List<Chamado> getChamados() {
