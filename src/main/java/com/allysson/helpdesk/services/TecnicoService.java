1e8d17d2a9cc530a5a7c7015cd31461884f2fc79
C1325053 <allysson.oliveira@capgemini.com>
2022-05-04 01:05:44 -0300

    endpoint findAll para Tecnicos

--- src/main/java/com/allysson/helpdesk/services/TecnicoService.java
+++ src/main/java/com/allysson/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.allysson.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,11 @@ public class TecnicoService {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id:" + id));
 		
+	}
+
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 
 }
